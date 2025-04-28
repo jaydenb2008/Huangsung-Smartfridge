af4e94bd4f1266fd85d691ae0b3533192f662802
jaydenb2008 <jjeromebui2008@gmail.com>
2025-04-28 13:07:10 -0700

    add drink entity unit test (passes)

---
+++ Common/src/main/java/edu/sdccd/cisc191/common/model/Drink.java
@@ -0,0 +1,31 @@
+package edu.sdccd.cisc191.common.model;
+
+import jakarta.persistence.Entity;
+
+import java.util.Date;
+
+@Entity
+public class Drink extends FoodItem {
+    private boolean isOpened;
+
+    public Drink() {
+        super();
+    }
+
+    public Drink(String name, String foodType, float quantityLeft, Date expirationDate, boolean isOpened) {
+        super(name, foodType, quantityLeft, expirationDate);
+        this.isOpened = isOpened;
+    }
+
+    public boolean getOpened() {
+        return isOpened;
+    }
+
+    public void setOpened() {
+        this.isOpened = isOpened;
+    }
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+}
